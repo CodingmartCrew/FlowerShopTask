73849d90e84ed226bc72220e6f979d912a9166fd
RagavanCodingmart <ragavan@codingmart.com>
2021-02-19 11:02:02 +0530

    track and login page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -9,20 +9,20 @@ const Login = () => {
         <div className={!showBlock?"hidden":"block"+"z-10 fixed bg-opacity-25 w-screen h-screen absolute top-0 bg-gray-900 "}>
             <div className="w-screen relative sm:w-screen md:w-5/12 lg:w-5/12 bg-gray-100 h-screen">
                 <div className="flex justify-center">
-                    <div  className="absolute top-5 right-14">
+                    <div  className="absolute top-5 right-10">
                         <button className="font-bold" onClick={
                             ()=>{setShowBlock(false);}
                         }>
                             &#10005;
                         </button>
                     </div>
-                    <div className=" absolute top-10 w-9/12">
+                    <div className=" absolute top-10 w-10/12">
                         <nav className="flex space-x-5 font-lora font-bold text-xl">
-                            <span className={login?"active":""+"text-gray-300"} onClick={()=>{
+                            <span className={login?"active":""+"text-gray-400"} onClick={()=>{
                                 setLogin(true)
                             }} 
                             >Log in</span>
-                            <span className={!login?"active":""+"text-gray-300"} onClick={()=>{
+                            <span className={!login?"active":""+"text-gray-400"} onClick={()=>{
                                 setLogin(false)
                             }} 
                             >Register</span>
@@ -61,13 +61,13 @@ const Logins=()=>{
                         <input className="p-1 rounded-md border-2 border-gray-200" type="email" name="mail" id="mail" placeholder=' '/>
                         <label className="relative text-sm -top-8 px-3" htmlFor="email">Email<sup className="text-red-400">*</sup></label>
                     </div>
-                    <div className="relative ">
+                    <div className="relative">
                         <input className="p-1 rounded-md border-2 border-gray-200" type="password" name="password" id="password" placeholder=' '/>
                         <label className="relative text-sm -top-8 px-3" htmlFor="password">Password<sup className="text-red-400">*</sup></label>
                     </div>
                     <div className="flex justify-between pb-2">
-                        <div className="flex whitespace-nowrap w-3/12">
-                            <input className="border-1 border-orange" type="checkbox" name="remember" id="remember"/>
+                        <div className="flex whitespace-nowrap w-4/12">
+                            <input className="" type="checkbox" name="remember" id="remember"/>
                             <span className="text-xs relative bottom-0.5">Remember me</span>
                         </div>
                         <div className="flex">
